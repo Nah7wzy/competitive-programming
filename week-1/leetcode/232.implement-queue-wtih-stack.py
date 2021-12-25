8aa05fbb43837d8b417c191c80d4da1ac7338898
Nah7wzy <tamnahom7@gmail.com>
2021-12-26 01:02:28 +0300

    queue using stack

---
+++ week-1/leetcode/232.implement-queue-wtih-stack.py
@@ -0,0 +1,16 @@
+class MyQueue:
+
+    def __init__(self):
+        self.arr=[]
+
+    def push(self, x: int) -> None:
+        self.arr.append(x)
+
+    def pop(self) -> int:
+        return self.arr.pop(0)
+
+    def peek(self) -> int:
+        return self.arr[0]
+
+    def empty(self) -> bool:
+        return len(self.arr)==0
