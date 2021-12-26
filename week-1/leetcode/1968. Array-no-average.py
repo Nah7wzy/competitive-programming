db0d8229c2e769aff5ac9ac60e594f774e29a22a
Nah7wzy <tamnahom7@gmail.com>
2021-12-26 11:45:00 +0300

    no average rearrange array

--- week-1/leetcode/1968. Array-no-average.py
+++ week-1/leetcode/1968. Array-no-average.py
@@ -1,11 +1,9 @@
-# if (nums[i-1]+nums[i+1])/2==nums[i]:
-#                 nums[i+1],nums[-1]=nums[-1],nums[i+1]
-def rearrangeArray(nums):
-    odd=[]
-    even=[]
-    for i in nums:
-        even.append(i) if i%2==0 else odd.append(i)
-    
-    return odd
+class Solution:
+    def rearrangeArray(self, nums):
+        for i in range (1,len(nums)-1):
+            if (nums[i-1]<nums[i]<nums[i+1])or (nums[i-1]>nums[i]>nums[i+1]):
+                nums[i+1],nums[i]=nums[i],nums[i+1]
+        return nums
 
-print(rearrangeArray([1,2,3,4,5]))
+x=Solution()
+print(x.rearrangeArray([1,2,3,4,5]))
