a176be147178e1fc297b1d007d24be2e7520d643
Nah7wzy <tamnahom7@gmail.com>
2022-04-20 14:28:23 +0300

    largest number

--- week-1/leetcode/179.largest-number.py
+++ week-1/leetcode/179.largest-number.py
@@ -1,15 +1,13 @@
+class Large(str):
+    def __lt__(a, b):
+        return a+b < b+a
+
+
 def largestNumber(nums):
-     # for i in range(len(nums)):
-        #     maxn=nums[i]
-        #     inofmax=i #to keep track of max and index of max
-        #     for j in range(len(nums)):
-        #         if i==j:
-        #             continue
-        #         if str(nums[j])[0]>str(maxn)[0]:
-        #             maxn=nums[j]
-        #             inofmax=j
-        #         elif str(nums[j][0])==str(maxn)[0]:
-        #             if len(str(nums[j]))>len(str(maxn)):
-        #                 maxn=nums[j]
-        #                 inofmax=j
-        #             elif len(str(nums[j]))==len(str(maxn)):
+    counter = 0
+    nums = [str(num) for num in nums]
+    nums.sort(key=Large, reverse=True)
+    res = "".join(nums)
+    for num in nums:
+        counter += 1 if num == '0' else 0
+    return res if counter != len(nums) else '0'
