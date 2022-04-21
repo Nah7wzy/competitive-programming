1a3d85681ee1ecbbd1c2f99368e6bae25ee5d6aa
Nah7wzy <tamnahom7@gmail.com>
2022-04-22 00:36:55 +0300

    pancake sorting

--- week-1/leetcode/969. pancake-sorting.py
+++ week-1/leetcode/969. pancake-sorting.py
@@ -1,22 +1,28 @@
 class Solution:
     def pancakeSort(self, arr):
-        answer=[]
-        for i in range(len(arr), 0, -1):
-            # print(i)
-            y=max(arr[:i])
-            
-            k=arr.index(y)
-            answer.append(k)
-            temp=arr[:(arr.index(y)+1)]
+        answer = []
+        # a pointer at the right to start saving spot from the larger number and keep it from further sorting
+        r = len(arr)-1
+        while r > 0:
+            x = arr.index(max(arr[:r+1]))
+            if x == r:  # if the array or the max number is already sorted
+                r -= 1
+                continue
+
+            temp = arr[:x+1]
             temp.reverse()
-            arr[:(arr.index(y)+1)]=temp
-            temp=arr[:i]
+            arr[:x+1] = temp  # reverse the array from the index of max
+
+            temp = arr[:r+1]
             temp.reverse()
-            arr[:i]=temp
-            # arr.reverse()
-            # answer.append(arr.index(y))
-            # print(arr)
+            # reverse the array to the right pointer / put it before the last max
+            arr[:r+1] = temp
+
+            answer.append(x+1)
+            answer.append(r)
+            r -= 1
         return answer
 
-x=Solution()
-print(x.pancakeSort([3,2,4,1]))
+
+x = Solution()
+print(x.pancakeSort([1, 2, 3]))
