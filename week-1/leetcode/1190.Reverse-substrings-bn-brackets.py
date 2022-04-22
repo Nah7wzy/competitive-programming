926874a1775e4116c302b8a1d6676886c5e9960e
Nah7wzy <tamnahom7@gmail.com>
2022-04-22 20:19:03 +0300

    reverse substrings

--- week-1/leetcode/1190.Reverse-substrings-bn-brackets.py
+++ week-1/leetcode/1190.Reverse-substrings-bn-brackets.py
@@ -1,28 +1,22 @@
 def reverseParentheses(s: str) -> str:
-    arr=[*s]
+    arr = [*s]
+    stack = []
     for i in range(len(arr)):
         # print(arr[i])
-        if arr[i]==')':
+        if arr[i] == ')':
             # print(arr[i])
-            for j in range(i,-1,-1):
-                # print(arr[j])
-                if arr[j]=='(':
-                    temp=arr[j:arr.index(')')+1]
-                    # print(temp)
-                    temp.reverse()
-                    temp.pop(0)
-                    temp.pop()
-                    # print(temp)
-                    arr[j:arr.index(')')+1]=temp
-                    # print(arr)
-                if '(' not in arr or ')' not in arr:
-                    print(arr)
-                    answer=''.join(str(e) for e in arr)
-                    return answer
+            temp = []
+            while stack and stack[-1] != "(":
+                temp += stack.pop()
+            stack.pop()
+            for x in temp:
+                stack.append(x)
+        else:
+            stack.append(arr[i])
+    return ''.join(str(e) for e in stack)
     # print(arr)
 
 
-
-reverseParentheses("(abcd)")
-print(reverseParentheses("(u(love)i)"))
-print(reverseParentheses("(ed(et(oc))el)"))
+print(reverseParentheses("n(ev(t)((()lfevf))yd)cb()"))
+# print(reverseParentheses("(u(love)i)"))
+# print(reverseParentheses("(ed(et(oc))el)"))
