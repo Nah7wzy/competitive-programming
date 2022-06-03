833d4751e1c7dbc728a9ba42ab9d93dac0d62769
Nahom Tamru <61546296+Nah7wzy@users.noreply.github.com>
2022-06-03 21:01:33 +0300

    Time: 73 ms (57.82%), Space: 14 MB (54.38%) - LeetHub

--- 1346-check-if-n-and-its-double-exist/1346-check-if-n-and-its-double-exist.py
+++ 1346-check-if-n-and-its-double-exist/1346-check-if-n-and-its-double-exist.py
@@ -1,11 +1,11 @@
 class Solution:
     def checkIfExist(self, arr: List[int]) -> bool:
-        seen = {}
+        seen = []
         for i in arr:
             if i * 2 in seen or i / 2 in seen:
                 return True
             else:
-                seen[i] = 1
+                seen.append(i)
                 
         return False
         
