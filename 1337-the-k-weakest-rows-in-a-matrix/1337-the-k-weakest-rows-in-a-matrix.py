5afa8f68b69e57de0a825752e9eea5f2bbfcb0cf
Nahom Tamru <61546296+Nah7wzy@users.noreply.github.com>
2022-06-04 08:55:39 +0300

    Time: 176 ms (29.90%), Space: 14.3 MB (48.93%) - LeetHub

--- 1337-the-k-weakest-rows-in-a-matrix/1337-the-k-weakest-rows-in-a-matrix.py
+++ 1337-the-k-weakest-rows-in-a-matrix/1337-the-k-weakest-rows-in-a-matrix.py
@@ -1,11 +1,24 @@
 class Solution:
     def kWeakestRows(self, mat: List[List[int]], k: int) -> List[int]:
-        minHeap = [(x.count(1), i) for i, x in enumerate(mat)]
+        minHeap = [(self.findOnes(x), i) for i, x in enumerate(mat)]
         heapify(minHeap)
         ans = []
+        print(minHeap)
         while k > 0:
             ans.append(heappop(minHeap)[1])
             k -= 1
-            
+        
         return ans
+    
+    #use binary search to find count of ones and heap for strength
+    def findOnes(self, arr):
+        left, right = 0, len(arr)
+        while left < right:
+            mid = left + (right - left) // 2
+            if arr[mid] == 1:
+                left = mid + 1
+            else:
+                right = mid
+                
+        return right
         
