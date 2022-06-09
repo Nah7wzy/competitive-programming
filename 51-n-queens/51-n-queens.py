d9ad098dde891346465bcdf817797fdb5caaf8fb
Nahom Tamru <61546296+Nah7wzy@users.noreply.github.com>
2022-06-09 15:22:51 +0300

    Time: 89 ms (57.32%), Space: 14.5 MB (43.81%) - LeetHub

---
+++ 51-n-queens/51-n-queens.py
@@ -0,0 +1,28 @@
+class Solution:
+    def solveNQueens(self, n: int) -> List[List[str]]:
+        results = []
+        board = [['.' for x in range(n)] for y in range(n)]
+        posdiag, negdiag, col = set(), set(), set()
+        
+        def place(i):
+            if i == n:
+                results.append([''.join(row) for row in board])
+                return 
+            
+            for j in range(n):
+                if j not in col and i+j not in posdiag and j-i not in negdiag:
+                    posdiag.add(i+j)
+                    negdiag.add(j-i)
+                    col.add(j)
+                    board[i][j] = 'Q'
+                    place(i+1)
+                    posdiag.remove(i+j)
+                    negdiag.remove(j-i)
+                    col.remove(j)
+                    board[i][j] = '.'
+                    
+        place(0)
+        return results
+                
+                    
+                
