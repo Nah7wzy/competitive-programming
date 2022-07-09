084444b233cc83017d1fe776754690e8af96e93a
Nahom Tamru <61546296+Nah7wzy@users.noreply.github.com>
2022-07-09 18:45:06 +0300

    Time: 104 ms (34.06%), Space: 14.2 MB (55.74%) - LeetHub

--- 496-next-greater-element-i/496-next-greater-element-i.py
+++ 496-next-greater-element-i/496-next-greater-element-i.py
@@ -1,21 +1,17 @@
 class Solution:
     def nextGreaterElement(self, nums1: List[int], nums2: List[int]) -> List[int]:
-        temp=[] #keep track of index of nums1 in nums 2
-        result=[]
-        for i in range(len(nums1)):
-            temp.append(nums2.index(nums1[i]))
-                
-        for i in range(len(temp)):
-            index=temp[i] #to start next loop from its index
-            value = nums1[i]
-            count=0 #check if it has been found or none exist
-            for j in range(index+1,len(nums2)):
-                if nums2[j] > value:
-                    count+=1
-                    result.append(nums2[j])
-                    break;
-                    
-            if count==0:
-                result.append(-1)
+        mono = []
+        match = {}
+        for i in nums2: #do a monotonic stack on nums2 and register next greater element in dictionary
+            while mono and i > mono[-1]:
+                match[mono.pop()] = i                
+            mono.append(i)
+            
+        while mono: #remaining elements has no greater element
+            match[mono.pop()] = -1
         
-        return result
+        res = []
+        for i in nums1:
+            res.append(match[i])
+            
+        return res
