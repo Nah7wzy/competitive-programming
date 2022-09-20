e5d60ae057abff2a1733a8870a86c979e6cdbce1
Nahom Tamru <61546296+Nah7wzy@users.noreply.github.com>
2022-09-20 21:19:07 +0300

    Time: 1640 ms (5.01%), Space: 57.7 MB (5.15%) - LeetHub

--- 200-number-of-islands/200-number-of-islands.py
+++ 200-number-of-islands/200-number-of-islands.py
@@ -4,17 +4,40 @@ class Solution:
         inbound = lambda x, y : 0 <= x < n and 0 <= y < m
         directions = [(0,1), (1,0), (0,-1), (-1,0)]
         
-        def dfs(i, j):
-            if not inbound(i, j) or grid[i][j] == '0':
-                return 
-            grid[i][j] = '0'
-            for x, y in directions:
-                dfs(i+x, y+j)
-        res = 0        
-        for i in range(n):
-            for j in range(m):
-                if grid[i][j] == '1':
-                    res += 1
-                    dfs(i, j)
-                    
+        parent = {}
+        rank = {}
+        for r in range(n): #initialize parent to itself and rank to 1 or 0
+            for c in range(m):
+                parent[(r,c)] = (r,c) 
+                rank[(r,c)] = 1 if grid[r][c] == '1' else 0
+                
+        def find(i, j):
+            if parent[(i,j)] == (i, j):
+                return (i, j)
+            parent[(i, j)] = find(parent[(i,j)][0], parent[(i,j)][1]) #track back parent and assign on return for each to avoid recomputation
+            return parent[(i,j)]
+        
+        def union(a, b):
+            a_par = find(a[0], a[1])
+            b_par = find(b[0], b[1])
+            
+            if a_par != b_par: #if they have different parents they can be united
+                if rank[a_par] < rank[b_par]:
+                    parent[a_par] = b_par
+                    rank[b_par] += rank[a_par]
+                    rank[a_par] = 0
+                else:
+                    parent[b_par] = a_par
+                    rank[a_par] += rank[b_par]
+                    rank[b_par] = 0
+        
+        for r in range(n):
+            for c in range(m):
+                for x, y in directions:
+                    new_r, new_c = r + x, c + y
+                    if inbound(new_r, new_c) and grid[new_r][new_c] == '1' and grid[r][c] == '1':
+                        union((new_r, new_c), (r, c))
+        res = 0
+        for val in rank.values(): #positive rank means its the representative parent
+            res += 1 if val else 0
         return res
