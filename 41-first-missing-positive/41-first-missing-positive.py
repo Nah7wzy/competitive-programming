5542824cffddc7697514d08ac08910df89b5af9d
Nahom Tamru <61546296+Nah7wzy@users.noreply.github.com>
2022-09-20 12:07:44 +0300

    Time: 914 ms (49.78%), Space: 27.2 MB (85.74%) - LeetHub

--- 41-first-missing-positive/41-first-missing-positive.py
+++ 41-first-missing-positive/41-first-missing-positive.py
@@ -1,12 +1,27 @@
 class Solution:
     def firstMissingPositive(self, nums: List[int]) -> int:
-        nums.sort()
-        first = 1
-        for num in nums:
-            if num > 0:
-                if num > first:
-                    return first
-                elif num == first:
-                    first += 1
-                    
-        return first
+        n = len(nums)
+        #bring positive integers to the beginning (to better handle marked indices later)
+        a, b = 0, 0
+        while b < n:
+            if nums[b] <= 0:
+                nums[a], nums[b] = nums[b], nums[a]
+                a += 1
+            b += 1
+            
+        for i in range(a, n): #track using nums values as index
+            target = nums[i] if nums[i] >= 0 else -nums[i]  #if negative its marked so take the positive of the number
+            target = target + a - 1
+            if target < n:
+                if nums[target] >= 0:
+                    nums[target] *= -1
+        
+        res = 1
+        for i in range(a, n):  #find the first unmarked index
+            if nums[i] < 0:
+                res += 1
+            else:
+                break
+        
+        return res
+        
