8cb5c66410178bf235d241928e3bcc22682e756c
Nahom Tamru <61546296+Nah7wzy@users.noreply.github.com>
2022-09-22 12:26:37 +0300

    Time: 68 ms (10.61%), Space: 13.8 MB (75.24%) - LeetHub

---
+++ 1038-binary-search-tree-to-greater-sum-tree/1038-binary-search-tree-to-greater-sum-tree.py
@@ -0,0 +1,21 @@
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+class Solution:
+    def bstToGst(self, root: TreeNode) -> TreeNode:
+        total = 0
+        def dfs(node):
+            nonlocal total
+            if not node:
+                return
+            dfs(node.right)
+            temp = node.val
+            node.val += total
+            total += temp
+            dfs(node.left)
+            
+        dfs(root)
+        return root
