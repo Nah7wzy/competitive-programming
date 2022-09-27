8feb2f764be0318c6876193d77ec8f0f553ef6c9
Nahom Tamru <61546296+Nah7wzy@users.noreply.github.com>
2022-09-28 00:38:12 +0300

    Time: 272 ms (5.28%), Space: 14 MB (34.85%) - LeetHub

--- 36-valid-sudoku/36-valid-sudoku.py
+++ 36-valid-sudoku/36-valid-sudoku.py
@@ -1,32 +1,17 @@
 class Solution:
     def isValidSudoku(self, board: List[List[str]]) -> bool:
-        #row check
+        def subbox(i, j):
+            return ( (j // 3 + 1) + ((i // 3) * 3) )
+            
+        digits = defaultdict(set) #keep the boxes where digits appear and if they appear in the same box again return false
         for i in range(9):
-            digits = set()
+            row, col = set(), set()
             for j in range(9):
-                if board[i][j] != '.' and board[i][j] in digits:
+                if (board[i][j] != '.' and (board[i][j] in row or subbox(i,j) in digits[board[i][j]])) or (board[j][i] != '.' and board[j][i] in col):
                     return False
-                digits.add(board[i][j])
-        #column check
-        for j in range(9):
-            digits = set()
-            for i in range(9):
-                if board[i][j] != '.' and board[i][j] in digits:
-                    return False
-                digits.add(board[i][j])
+                digits[board[i][j]].add(subbox(i,j))
+                row.add(board[i][j])
+                col.add(board[j][i])             
                 
-        #sub-box check
-        x = y = 0
-        while x < 9:
-            digits = set()
-            for i in range(x, x+3):
-                for j in range(y, y+3):
-                    if board[i][j] != '.' and board[i][j] in digits:
-                        return False
-                    digits.add(board[i][j])
-            y += 3
-            if y >= 9:
-                y = 0
-                x += 3
         return True
         
