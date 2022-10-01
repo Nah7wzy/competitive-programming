7ce26d266e425ab05f2419cb50996cc545e6f542
Nahom Tamru <61546296+Nah7wzy@users.noreply.github.com>
2022-10-01 11:20:06 +0300

    Time: 847 ms (15.02%), Space: 14.1 MB (23.59%) - LeetHub

--- 1031-maximum-sum-of-two-non-overlapping-subarrays/1031-maximum-sum-of-two-non-overlapping-subarrays.py
+++ 1031-maximum-sum-of-two-non-overlapping-subarrays/1031-maximum-sum-of-two-non-overlapping-subarrays.py
@@ -1,27 +1,18 @@
 class Solution:
     def maxSumTwoNoOverlap(self, nums: List[int], firstLen: int, secondLen: int) -> int:
-        for i in range(1, len(nums)):
-            nums[i] = nums[i-1] + nums [i]
-        
-        ptr1, ptr2 = 0, firstLen
-        maxSum = 0
-        
-        while ptr2<=len(nums):
-            currSum = (nums[ptr2-1] - nums[ptr1-1]) if ptr1 != 0 else (nums[ptr2-1])
-            currWin = (ptr1, ptr2)
-            ptra, ptrb = 0, secondLen
+        ps = [0]
+        for i in range(len(nums)):
+            ps.append(ps[-1] + nums[i])
             
-            while ptrb<=len(nums):
-                if ptra <= ptr2-1 and ptrb-1 >= ptr1: #check window overlap
-                    pass
-                else:
-                    checkSum = (nums[ptrb-1] - nums[ptra-1]) if ptra != 0 else (nums[ptrb-1])
-                    maxSum = max(maxSum, (currSum + checkSum))
-                    
-                ptra+=1
-                ptrb+=1
-                
-            ptr1+=1
-            ptr2+=1
+        a1, b1, a2, b2 = 1, firstLen, 1, secondLen
+        res = 0
+        while b1 < len(ps):
+            while b2 < len(ps):
+                if b2 < a1 or a2 > b1:  #no overlap
+                    cur_sum = (ps[b1] - ps[a1 - 1]) + (ps[b2] - ps[a2 - 1])
+                    res = max(res, cur_sum)
+                a2, b2 = a2 + 1, b2 + 1
+            a2, b2 = 1, secondLen
+            a1, b1 = a1 + 1, b1 + 1
             
-        return maxSum
+        return res
