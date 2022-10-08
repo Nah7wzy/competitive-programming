46c4e480c38a7535ef03584d02db996411675847
Nahom Tamru <61546296+Nah7wzy@users.noreply.github.com>
2022-10-08 19:47:34 +0300

    Time: 1727 ms (15.24%), Space: 30.4 MB (25.82%) - LeetHub

--- 131-palindrome-partitioning/131-palindrome-partitioning.py
+++ 131-palindrome-partitioning/131-palindrome-partitioning.py
@@ -2,12 +2,18 @@ class Solution:
     def partition(self, s: str) -> List[List[str]]:
         res = []
         n = len(s)
+        dp = {}
         def backtrack(i, partition):
             if i >= n:
                 res.append(partition.copy())
                 return
             for j in range(i, n):
-                if self.isPalindrome(s, i, j):
+                if (i, j) in dp:
+                    valid = dp[(i,j)]
+                else:
+                    valid = self.isPalindrome(s, i, j)
+                    dp[(i,j)] = valid
+                if valid:
                     partition.append(s[i:j+1])
                     backtrack(j + 1, partition)
                     partition.pop()
