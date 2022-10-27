bb3dcfce591fe5af7cdad47b3ee76e2fc184f7da
Nahom Tamru <61546296+Nah7wzy@users.noreply.github.com>
2022-10-27 15:48:38 +0300

    Time: 456 ms (27.68%), Space: 17.6 MB (60.33%) - LeetHub

--- 0072-edit-distance/0072-edit-distance.py
+++ 0072-edit-distance/0072-edit-distance.py
@@ -1,27 +1,15 @@
 class Solution:
     def minDistance(self, word1: str, word2: str) -> int:
-        #use i, j index as state for the dp and try out the three options
-        #insert move j only, delete move i only, replace move both - add 1 for all three; match move both without adding
-        memo = {}
-        def dp(i, j):
-            if i >= len(word1): #need to insert remaining
-                return len(word2) - j
-            if j >= len(word2): #need to delete remaining
-                return len(word1) - i
-            if i >= len(word1) and j >= len(word2): 
-                return 0
-            
-            if (i, j) in memo:
-                return memo[(i, j)]
-            
-            if word1[i] == word2[j]:
-                return dp(i+1, j+1)
-            
-            operations = 1
-            operations += min(dp(i, j+1), dp(i+1, j), dp(i+1, j+1))
-            memo[(i,j)] = operations
-            
-            return memo[(i,j)]
+        dp = [[j for _ in range(len(word1) + 1)] for j in range(len(word2), 0, -1)]
+        dp.append([i for i in range(len(word1), -1, -1)])
         
-        return dp(0, 0)
+        for i in range(len(word2) - 1, -1, -1):
+            for j in range(len(word1) -1, -1, -1):
+                if word2[i] == word1[j]:
+                    dp[i][j] = dp[i+1][j+1]
+                else:
+                    dp[i][j] = 1 + min(dp[i+1][j], dp[i][j+1], dp[i+1][j+1])
+
+        return dp[0][0]
+        
         
