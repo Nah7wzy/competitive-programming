8052e4ab86510f739eba3f90c95c9a001dd772fe
Nahom Tamru <61546296+Nah7wzy@users.noreply.github.com>
2023-02-16 22:13:10 +0300

    Time: 1551 ms (45.82%), Space: 28.9 MB (99.37%) - LeetHub

---
+++ 2007-find-original-array-from-doubled-array/2007-find-original-array-from-doubled-array.py
@@ -0,0 +1,28 @@
+class Solution:
+    def findOriginalArray(self, changed: List[int]) -> List[int]:
+        changed.sort()
+        # l points at the smallest element, r points at the doubled elements
+        l, r = 0, 1
+        result = []
+        while l < len(changed):
+            # r must always be greater than l
+            if r <= l: r = l + 1
+
+            if changed[l] == -1:
+                l += 1
+                continue
+
+            # move r right until it is double of l
+            while r < len(changed) and changed[r] != changed[l] * 2:
+                r += 1
+            # couldn't find a corresponding doubled element
+            if r == len(changed): return []
+
+            # flag r as used
+            changed[r] = -1
+            result.append(changed[l])
+
+            l += 1
+            
+        return result
+
